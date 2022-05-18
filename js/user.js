d43f6647184320030d8b093d70eabad843c943e7
Booflame <Loekken.n@gmail.com>
2022-05-18 08:34:28 +0200

    skubber

---
+++ js/user.js
@@ -0,0 +1,10 @@
+fetch("js/users.json")
+    .then(response => response.json())
+    .then(json => writeData(json))
+    .catch((error) => {console.error("Error:", error)});
+
+function writeData(data){
+    data.users.forEach( e => {
+        console.log(e.username)
+    });
+}
