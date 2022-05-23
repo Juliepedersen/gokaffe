2048692862b8c6792c16eb90f935fc6337625ff9
Booflame <Loekken.n@gmail.com>
2022-05-23 14:02:59 +0200

    footer opstart

--- js/burger.js
+++ js/burger.js
@@ -1,22 +1,22 @@
-const btn = document.querySelector(".burger");
-const nav = document.querySelector(".primary-nav");
-const links = document.querySelectorAll(".link");
+const btn = document.querySelector(".burger")
+const nav = document.querySelector(".primary-nav")
+const links = document.querySelectorAll(".link")
 // Open and close navigation when pressing button
 btn.addEventListener("click", () => {
     // Checking the value of visible every time there is clicked
-    const visible = nav.getAttribute("data-visible");
+    const visible = nav.getAttribute("data-visible")
     if (visible === "false") {
-        nav.setAttribute("data-visible", true);
-        btn.setAttribute("aria-expanded", true);
+        nav.setAttribute("data-visible", true)
+        btn.setAttribute("aria-expanded", true)
     } else if (visible === "true") {
-        nav.setAttribute("data-visible", false);
-        btn.setAttribute("aria-expanded", false);
+        nav.setAttribute("data-visible", false)
+        btn.setAttribute("aria-expanded", false)
     }
-});
+})
 // Close navigation when pressing links
-links.forEach(function(link){
+links.forEach(link => {
     link.addEventListener("click", () => {
-        nav.setAttribute("data-visible", false);
-        btn.setAttribute("aria-expanded", false);
+        nav.setAttribute("data-visible", false)
+        btn.setAttribute("aria-expanded", false)
     })
-});
+})
--- js/headerObserver.js
+++ js/headerObserver.js
@@ -3,6 +3,8 @@ const header = document.querySelector("header")
 
 const headerObs = new IntersectionObserver(entries => 
     header.classList.toggle('hero-nav', entries[0].isIntersecting),
-{threshold: "0.3"})
+{
+    threshold: "0.3"
+})
 
 headerObs.observe(hero)
