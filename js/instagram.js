f78e5b31d58fc2021bd6e3e8b556c281ea1dfc09
Booflame <Loekken.n@gmail.com>
2022-05-25 00:23:13 +0200

    javascript der ikke duer ;(

--- js/instagram.js
+++ js/instagram.js
@@ -2,18 +2,27 @@ let currentIndex = 0;
 const prev = document.querySelector("#prev-image")
 const next = document.querySelector("#next-image")
 const images = document.querySelector(".image-list")
+const slide = document.querySelectorAll("[data-index]")
 
 prev.addEventListener("click", () => {
-    currentIndex--;
+    currentIndex--
     if(currentIndex < 0) {
-        currentIndex = images.children.length;
+        currentIndex = images.children.length - 1
+    }
+    // console.log(images.children[currentIndex])
+    for(let i = 0; i < images.children.length; i++){
+        console.log(images.children[i+1])
     }
     console.log(currentIndex)
 })
 next.addEventListener("click", () => {
-    currentIndex++;
-    if(currentIndex > images.children.length) {
-        currentIndex = 0;
+    if(currentIndex >= images.children.length) {
+        currentIndex = 0
     }
-    console.log(currentIndex)
+    for(let i = 0; i < images.children.length; i++){
+        images.children[i].setAttribute("data-index", `${i + 1}`)
+        console.log(images.children[i])
+    }
+    currentIndex++
+    // console.log(currentIndex)
 })
